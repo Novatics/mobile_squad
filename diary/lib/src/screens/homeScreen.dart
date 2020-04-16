65356f4bb8a24ef57390bf40381ad0deb219450d
VitorRibeiroCustodio <vitorribeiro221196@gmail.com>
2020-04-16 11:59:37 -0300

    Adding font and radius top

--- diary/lib/src/screens/homeScreen.dart
+++ diary/lib/src/screens/homeScreen.dart
@@ -4,39 +4,22 @@ import '../widgets/diaryItem/diaryItem.dart';
 class HomeScreen extends StatelessWidget {
   Widget build(context) {
 
-    return ClipPath(
-      child: 
+    return 
       Container(
-          color: Color.fromRGBO(248, 248, 248, 1),
           margin: EdgeInsets.only(top: 20.0),
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height,
           child: Column(
             children: <Widget>[
+              Container(margin: EdgeInsets.only(top: 15.0)),
               DiaryItem(),
               DiaryItem(),
             ],
           ),
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(248, 248, 248, 1),
+            borderRadius: BorderRadius.only(topLeft: Radius.circular(200.0), topRight: Radius.circular(200.0) )
         ),
     );
-  }
-}
-
-class backGroundClipper extends CustomClipper<Path>{
-
-  @override
-  Path getClip(Size size) {
-    Path p = Path();
-
-    p.moveTo(size.width / 2, size.height / 2);
-    p.quadraticBezierTo(size.width / 2, 0, size.width / 2, 0);
-
-
-    return p;
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) {
-    return false;
   }
 }
